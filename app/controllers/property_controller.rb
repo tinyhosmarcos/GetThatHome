7f95c32287f9a366f4e1a5f95a32b9381db24f35
Alex Mamani <alex.mamani@tecsup.edu.pe>
2022-07-13 10:55:53 -0500

    CHORE: property controller

---
+++ app/controllers/property_controller.rb
@@ -0,0 +1,48 @@
+class PropertyController < ApplicationController
+  skip_before_action :authorize
+  # , only: :index
+
+
+  def index
+    @properties = Property.all
+    render json: @properties
+  end
+
+  def show
+    @property = Property.find(params[:id])
+    render json: @property
+    # , only: [name, email, phone]
+  end
+
+
+  def create
+    property = Property.new(property_params)
+    if property.save
+      render json: {status: 'SUCCESS', message: 'Saved property', data: property}, status: :ok
+    else
+      render json: {status: 'ERROR', message: 'Property not saved', data: property.errors}, status: :unprocessable_entity
+    end
+  end
+
+
+  def destroy
+    property = Property.find(params[:id])
+    render json: {status: 'SUCCESS', message: 'Deleted property', data: property}, status: :ok
+  end
+
+  ### falta probar
+  def update
+    property = Property.find(params[:id])
+    if property.update_attribute(property_params)
+      render json: { status: 'SUCCESS', message: 'Updated property', data: property }, status: :ok
+    else
+      render json: { status: 'ERROR', message: 'Property not updated', data: property.errors }, status: :unprocessable_entity
+    end
+  end
+
+  ### falta probar
+  private
+  def property_params
+    params.permit(:operation_type, :address, :monthly_price, :property_type, :bedrooms_count, :bathrooms_count, :area, :description, :active_published, :user_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   post "/login" => "sessions#create"
   get "/logout" => "sessions#destroy"
+
+  get "/user" => "user#index"
+  get "/user/:id" => "user#show"
+
+  get "property" => "property#index"
+  get "property/:id" => "property#show"
+  
+  post "property" => "property#create"
+  delete "property/:id" => "property#destroy"
+  
+  ### falta 
+  patch "property/:id" => "property#update" 
 end
