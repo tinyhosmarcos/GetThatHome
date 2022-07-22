b8327d4e621e9181d68ad70b3d8febd9ad27774d
Yhostin Ollachica <yhostin.ollachica@ucsp.edu.pe>
2022-07-22 13:56:05 -0500

    CHORE: ADD status model

---
+++ app/models/status.rb
@@ -0,0 +1,5 @@
+class Status < ApplicationRecord
+  belongs_to :user
+  belongs_to :property
+  enum status_type: { favorite: 0, contact: 1 }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_21_042519) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_22_054929) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_21_042519) do
     t.index ["user_id"], name: "index_properties_on_user_id"
   end
 
+  create_table "statuses", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "property_id", null: false
+    t.integer "status_type", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["property_id"], name: "index_statuses_on_property_id"
+    t.index ["user_id"], name: "index_statuses_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email", null: false
@@ -77,4 +87,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_21_042519) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "properties", "users"
+  add_foreign_key "statuses", "properties"
+  add_foreign_key "statuses", "users"
 end
